d1c8b9e508ddda96a20a52b3fd1e6988646efe2b
X Archive <amshdq@126.com>
2018-07-09 13:41:59 +0800

    modify first.sh

--- scripts/first.sh
+++ scripts/first.sh
@@ -1,1 +1,7 @@
-The first scripts
+#！/bin/bash
+#This is a testing scripts
+#Auth Bob In BJ 2018.07.09
+#first.sh
+echo "The first scripts"
+echo "if you see it,that tell you can access github response by
+internet"
